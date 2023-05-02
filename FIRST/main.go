32efaf95abbc0fe2d87d069f71de2576b8dc20ef
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-05-02 20:01:21 +0700

    Go run and build

---
+++ FIRST/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"first/learning"
+	"fmt"
+)
+
+func main() {
+	subject := learning.Subject{Id: 1, Title: "Belajar Golang"}
+
+	fmt.Println(learning.Id)
+	fmt.Println(learning.Title)
+
+}
