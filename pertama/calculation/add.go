3d5d11205b6f2c872af945a637add81ceef958b7
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-05-02 20:31:45 +0700

    Package lebih lanjut

---
+++ pertama/calculation/add.go
@@ -0,0 +1,11 @@
+package calculation
+
+func Add(number int, numbertwo int) int {
+	return add(5, 3)
+
+}
+
+func add(number int, numbertwo int) int {
+	return number + numbertwo
+
+}
--- pertama/main.go
+++ pertama/main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"pertama/calculation"
+)
 
 func main() {
 	fmt.Println("hallo, Belajar Golang")
-	sentence := TestAja()
+	/*sentence := TestAja()*/
 
-	fmt.Println(sentence)
+	/*fmt.Println(sentence) */
+	fmt.Println("--------")
+
+	result := calculation.Add(5, 5)
+	fmt.Println(result)
 }
 
 /*go build */
