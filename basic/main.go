a9fdadaedf238e93672e176a9722ba786e9bff3b
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-05-03 09:37:16 +0700

    Variable, type data, and nilai default

---
+++ basic/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Golang"
+	fmt.Println(name)
+
+	var name2 string
+	name2 = "Ruby on Rails"
+	fmt.Println(name2)
+
+	age := 23
+	fmt.Println(age)
+
+}
